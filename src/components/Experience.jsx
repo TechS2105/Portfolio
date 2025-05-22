893fc938081ac9718752087ae2fd76c951d1d288
Sovan Sundar Dey <techsovan.github0521@gmail.com>
2025-05-23 00:07:05 +0530

    modify Experience.module.css and Experience.jsx file code

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,20 +1,97 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Container from './Container';
 import ExperienceStyle from '../../public/styles/Experience.module.css';
 import ExperienceTxt from '../objects/Experiencetxt';
 import { FaHandPointRight } from "react-icons/fa6";
 
 function Experience() {
+
+    const [headingScrollStyle, setHeadingScrollStyle] = useState({
+
+        transform: "translateY(-500px)",
+        filter: "blur(50px)"
+
+    });
+
+    const [companyNameAnimation, setCompanyNameAnimation] = useState({
+
+      transform: "translateY(500px)",
+      filter: "blur(20px)",
+      position: "absolute",
+      content: "",
+      top: "50px",
+      left: "0",
+      height: "3px",
+      width: "100%",
+      backgroundColor: "#efcb55",
+      
+    });
+
+    function handelExperienceScroll() {
+        
+        if(window.innerWidth > 600){
+
+            if (window.scrollY > 1300) {
+                
+                setHeadingScrollStyle({
+
+                    transform: "translateY(0px)",
+                    transition: "all 0.8s ease",
+                    filter: "blur(0px)",
+
+                });
+
+                setCompanyNameAnimation({
+
+                    transform: "translateY(0px)",
+                    transition: "all 0.8s ease",
+                    filter: "blur(0px)",
+
+                });
+
+            } else {
+                
+                setHeadingScrollStyle({
+
+                    transform: "translateY(-500px)",
+                    transition: "all 0.8s ease",
+                    filter: "blur(50px)"
+
+                });
+
+                setCompanyNameAnimation({
+
+                    transform: "translateY(500px)",
+                    transition: 'all 0.8s ease',
+                    filter: "blur(20px)",
+
+                })
+
+            }
+
+        }
+
+    }
+
+    useEffect(() => {
+
+        setTimeout(() => {
+
+            window.addEventListener("scroll", handelExperienceScroll);
+
+        })
+
+    }, [])
     
     return (
 
         <Container>
 
-            <div id="experience" className={ExperienceStyle.experienceContainer}>
+            <div id="experience" className={ExperienceStyle.experienceContainer} onScroll={handelExperienceScroll}>
 
                 <div className={ExperienceStyle.experienceHeading}>
 
-                    <h2>
+                    <h2 style={headingScrollStyle}>
                     Experience <span className={ExperienceStyle.experienceHeadingSpan}> & Role </span> </h2>
 
                 </div>
@@ -24,7 +101,7 @@ function Experience() {
 
                         <div className={ExperienceStyle.experienceContentText}>
                             
-                            <h3> {ExperienceTxt.companyName}  </h3>
+                            <h3 style={companyNameAnimation}> {ExperienceTxt.companyName}  </h3>
                         
                         </div>
                         <div className={ExperienceStyle.experienceContentText}>
