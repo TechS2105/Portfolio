9aee29f27a4773dbb2dd418de6174462d88fc677
Sovan Sundar Dey <techsovan.github0521@gmail.com>
2025-05-25 02:02:28 +0530

    modify codes for Contact.module.css and Contact.jsx files

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,95 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Container from './Container';
 import ContactStyle from '../../public/styles/Contact.module.css';
 
 function Contact() {
 
+    const [contactHeadingAnime, setContactHeadingAnime] = useState({
+
+        transform: "translateY(-500px)",
+        filter: "blur(20px)",
+
+    });
+
+    const [contactInfoAnime, setContactInfoAnime] = useState({
+
+        transform: "translateY(1000px)",
+        filter: "blur(20px)",
+        
+    });
+
+    function handelContactScroll() {
+        
+        if (window.innerWidth > 600) {
+            
+            if (window.scrollY > 5500) {
+                
+                setContactHeadingAnime({
+
+                    transform: "translateY(0px)",
+                    filter: "blur(0px)",
+                    transition: "all 0.8s ease",
+
+                });
+
+                setContactInfoAnime({
+
+                    transform: "translateY(0px)",
+                    filter: "blur(0px)",
+                    transition: "all 0.8s ease 0.5s",
+
+                });
+
+            } else {
+                
+                setContactHeadingAnime({
+
+                    transform: "translateY(-500px)",
+                    filter: "blur(20px)",
+                    transition: "all 0.8s ease",
+
+                });
+
+                setContactInfoAnime({
+
+                    transform: "translateY(1000px)",
+                    filter: "blur(20px)",
+                    transition: "all 0.8s ease",
+
+                });
+
+
+            }
+
+        }
+
+    }
+
+    useEffect(() => {
+
+        setTimeout(() => {
+
+            window.addEventListener("scroll", handelContactScroll);
+
+        })
+
+    }, []);
+
     return (
 
         <Container>
 
-            <div id="contact" className={ContactStyle.contactContainer}>
+            <div id="contact" className={ContactStyle.contactContainer} onScroll={handelContactScroll}>
                 
                 <div className={ContactStyle.contactHeading}>
 
-                    <h2> Get In <span className={ContactStyle.spanContactHeading}> Touch</span></h2>
+                    <h2 style={contactHeadingAnime}> Get In <span className={ContactStyle.spanContactHeading}> Touch</span></h2>
 
                 </div>
 
                 <div className={ContactStyle.contactInformationContainer}>
 
-                    <div className={ContactStyle.contactDetails}>
+                    <div className={ContactStyle.contactDetails} style={contactInfoAnime}>
 
                         <div className={ContactStyle.contactsInfo}>Memari, Burdwan</div>
                         <div className={ContactStyle.contactsInfo}>+91 954XX XX494</div>
@@ -27,7 +98,7 @@ function Contact() {
                     </div>
                     <div className={ContactStyle.contactMap}>
                             
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29342.240055881757!2d88.08314294547017!3d23.178228012110967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f859e1236f5c91%3A0x87bd2167faca5f0f!2z4Kau4KeH4Kau4Ka-4Kaw4Ka_LCDgppPgpq_gprzgp4fgprjgp43gpp8g4Kas4KeH4KaZ4KeN4KaX4Kay!5e0!3m2!1sbn!2sin!4v1748079013678!5m2!1sbn!2sin" style={{width: "100%", height: "100%", border: "0"}}loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29342.240055881757!2d88.08314294547017!3d23.178228012110967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f859e1236f5c91%3A0x87bd2167faca5f0f!2z4Kau4KeH4Kau4Ka-4Kaw4Ka_LCDgppPgpq_gprzgp4fgprjgp43gpp8g4Kas4KeH4KaZ4KeN4KaX4Kay!5e0!3m2!1sbn!2sin!4v1748079013678!5m2!1sbn!2sin" style={{width: "100%", height: "100%", border: "0"}} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
 
                     </div>
                     <div className={ContactStyle.contactForm}>  
