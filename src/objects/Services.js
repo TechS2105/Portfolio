251d4b4d080c7dd0921d8c56d046f92fd7cdfb2a
Sovan Sundar Dey <techsovan.github0521@gmail.com>
2025-05-24 13:55:38 +0530

    remove ../../public path from Service.js file and replace with only /image path

--- src/objects/Services.js
+++ src/objects/Services.js
@@ -2,7 +2,7 @@ const Services = [
   {
     id: 1,
     servicename: "Front-End Development",
-    serviceicon: "../../public/images/ux-design.png",
+    serviceicon: "/images/ux-design.png",
     description:
       "My Front-End Development service creates responsive, visually engaging, and user-centric websites for seamless cross-device experiences. Using HTML for structure, CSS for design, and JavaScript for interactivity—enhanced by modern frameworks like React, I build fast, accessible interfaces. Whether launching or upgrading, we craft digital experiences that elevate your brand and captivate your audience.",
   },
@@ -10,7 +10,7 @@ const Services = [
   {
     id: 2,
     servicename: "Back-End Development",
-    serviceicon: "../../public/images/backend-coding.png",
+    serviceicon: "/images/backend-coding.png",
     description:
       "My Back-End Development service delivers powerful, secure, and scalable server-side solutions that drive seamless functionality and performance. I specialize in languages like Node.js, ExpressJS, My SQL and PostgresSQL to manage databases, APIs, and application logic. Whether building from scratch or enhancing existing systems, I ensure your digital infrastructure runs smoothly, efficiently, and reliably behind the scenes.",
   },
@@ -18,7 +18,7 @@ const Services = [
   {
     id: 3,
     servicename: "Full Stack Development",
-    serviceicon: "../../public/images/coding.png",
+    serviceicon: "/images/coding.png",
     description:
       "My Fullstack Development service delivers complete web solutions by integrating dynamic front-end interfaces with robust back-end logic and efficient database management. I use HTML, CSS, and JavaScript with frameworks like React for the front end; Node.js with Express on the back end; plus MySQL, and PostgreSQL for reliable data storage ensuring seamless, scalable, and secure applications.",
   },
@@ -26,7 +26,7 @@ const Services = [
   {
     id: 4,
     servicename: "Wordpress Development",
-    serviceicon: "../../public/images/wordpress.png",
+    serviceicon: "/images/wordpress.png",
     description:
       "Elevate your online presence with our WordPress Development service, delivering bespoke, responsive websites crafted to perfection. From custom theme design and plugin integration to performance optimization and robust security, we ensure a scalable, SEO-friendly platform tailored to your brand’s unique needs. I intuitive content management solutions make updates effortless, empowering you to grow with confidence and stand out in the digital realm.",
   },
