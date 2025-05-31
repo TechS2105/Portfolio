506ecb71135ca57f9d24568be4a41683a67842d3
Sovan Sundar Dey <techsovan.github0521@gmail.com>
2025-05-31 12:51:39 +0530

    modify App.jsx file code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../public/styles/App.css';
 import Navbar from './components/Navbar';
-// import ResponsiveNav from './responsive_components/Responsivenav';
+import ResponsiveNav from './responsive_components/Responsivenav';
 import SiteLoader from './components/Siteloader';
 import Home from './components/Home';
 import About from './components/About';
@@ -22,7 +22,7 @@ function App() {
       
       <SiteLoader />
       <Navbar />
-      {/* <ResponsiveNav /> */}
+      <ResponsiveNav />
       <Home />
       <About />
       <Experience />
